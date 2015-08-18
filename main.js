882ef2f2e3aab65f8cbcc91b6f1ff0c01e6e7bb5
Steve Judd <sejudd1@github.com>
2015-08-18 14:44:31 -0700

    initial commit for reworked TicTacToe

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
-var player = 0;
-var boxes = document.querySelectorAll("li");
+
+var player = 1;
+var boxes = document.querySelectorAll(".box");
 var turns = 0;
 var button = document.querySelector("button");
-var array = [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6];
+var array = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[1,4,7],[2,5,8],[0,3,6],[1,4,7],[2,5,8]];
 
-document.button.querySelector("reset").addEventListener.("click", resetBoard);
+document.querySelector("#reset").addEventListener("click", resetBoard);
 
 // Listening for clicks on board and setting player 
 
 for(var i = 0; i < boxes.length; i++) {
-	boxes[i].addEventListener("click", getPlayer);
+	boxes[i].addEventListener("click", getPlayer)
 
 }
 
-function getPlayer() {
+function getPlayer(){
 	
-		if (this.innerHTML !== "X" && this.innerHTML !== "O"){
-			
-			if(turns % 2 === 0){
-		
-				this.innerHTML="X";
+		if(!this.innerHTML){
 
-			}else {
-	    
-	    		this.innerHTML="O";
+				if(turns % 2 === 0){
 
-			}
-			isWinner();
-			turns ++;
-			
+					this.innerHTML = "X";
+					player = 1;
+
+				} else {
+
+					this.innerHTML = "O";
+					player = 2;
+				}
+				
+				turns++;
+			isWinner(this.innerHTML)
 		}
 
 }
@@ -36,21 +38,21 @@ function getPlayer() {
 // Resets the board
 function resetBoard(){
 	for(var i = 0; i < boxes.length; i++){
-		boxes[i].innerHTML="";
+		boxes[i].innerHTML = "";
 	}
 }
-// determines winner
-function isWinner(){
-	for (j = 0; j < array.length; j++){
-		
-		if(boxes[array][j][0].innerHTML === player && 
-			boxes[array][j][1].innerHTML === player && 
-			boxes[array][j][2].innerHTML === player){
-			
-			return player + "X's Win";
-		}
 
-		
+
+
+// Determines winner
+function isWinner(player){
+	for(var i = 0; i < array.length; i++){
+		if(boxes[array[i][0]].innerHTML === player && boxes[array[i][1]].innerHTML === player && boxes[array[i][2]].innerHTML === player){
 			
-	}
+			console.log(player + " Wins!");
+
+		}
+	}		
+			
+}
 	
